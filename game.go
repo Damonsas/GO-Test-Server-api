eccb52e84c6863287c93b7ef0a9466b21a129a02
Damonsas <danh.kh.pro@gmail.com>
2025-05-30 02:50:46 +0200

    fixed

--- game.go
+++ game.go
@@ -2,33 +2,39 @@ package main
 
 import (
 	"html/template"
-	"log"
 	"net/http"
 )
 
 // Exercice 4
 
 type CompleteParoleData struct {
-	Message string
-	Class   string
+	Message1 string
+	Class    string
+	Message2 string
 }
 
 func CompleteParoleHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("game handler called")
-
 	data := CompleteParoleData{}
 	if r.Method == http.MethodPost {
 		answer1 := r.FormValue("answer1")
 		answer2 := r.FormValue("answer2")
-		if answer1 == "want" && answer2 == "oui qu'il n'y a" {
-			data.Message = "Incroyable !"
-			data.Class = "success"
-		} else if answer1 == "want" || answer2 == "oui qu'il n'y a" {
-			data.Message = "bonne réponse"
-			data.Class = "success"
-		} else {
-			data.Message = "mauvaise réponse"
-			data.Class = "error"
+		if answer1 != "" {
+			if answer1 == "want" {
+				data.Message1 = "bonne réponse"
+				data.Class = "success"
+			} else {
+				data.Message1 = "mauvaise réponse"
+				data.Class = "error"
+			}
+		}
+		if answer2 != "" {
+			if answer2 == "oui qu'il n'y a" {
+				data.Message2 = "bonne réponse"
+				data.Class = "success"
+			} else {
+				data.Message2 = "mauvaise réponse"
+				data.Class = "error"
+			}
 		}
 	}
 	tmpl := template.Must(template.ParseFiles("pages/game.html"))
